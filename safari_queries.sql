d00699d9a252f09e78bb8f412292dbd7dc1a42aa
amylianne <amywynn.3.a@gmail.com>
2022-12-01 16:25:53 +0000

    extension q1 added

--- safari_queries.sql
+++ safari_queries.sql
@@ -1,3 +1,8 @@
-SELECT * FROM animal WHERE enclosureid = 1;
---SELECT * FROM assignment INNER JOIN staff ON assignment.id = assignment.staffid;
-SELECT * FROM staff LEFT JOIN assignment ON staff.id = assignment.staffid WHERE assignment.enclosureid = 1;
+--SELECT * FROM animal WHERE enclosureid = 1;
+--SELECT * FROM staff LEFT JOIN assignment ON staff.id = assignment.staffid WHERE assignment.enclosureid = 1;
+
+SELECT staff.name, staff.id AS staffNumber, assignment.staffid, enclosure.name AS animalEnclosure, closedForMainenance 
+FROM staff LEFT JOIN assignment ON staff.id = assignment.staffid
+LEFT JOIN enclosure 
+ON assignment.enclosureid = enclosure.id 
+WHERE closedForMainenance = true;
