0826c1348032f28997bcb571099d034f4b3c0e47
amylianne <amywynn.3.a@gmail.com>
2022-12-01 15:21:30 +0000

    tables done

--- safari.sql
+++ safari.sql
@@ -48,3 +48,33 @@ INSERT INTO enclosure (name, capacity, closedForMainenance) VALUES ('Elephant En
 INSERT INTO enclosure (name, capacity, closedForMainenance) VALUES ('Tiger enclosure', 4, true);
 INSERT INTO enclosure (name, capacity, closedForMainenance) VALUES ('Lion enclosure', 6, false);
 INSERT INTO enclosure (name, capacity, closedForMainenance) VALUES ('Meerkat enclosure', 100, true);
+
+-- Create assignment
+
+INSERT INTO assignment (employeeid, enclosureid, day) VALUES (1, 1, 'Monday');
+INSERT INTO assignment (employeeid, enclosureid, day) VALUES (1, 1, 'Tuesday');
+INSERT INTO assignment (employeeid, enclosureid, day) VALUES (2, 2, 'Tuesday');
+INSERT INTO assignment (employeeid, enclosureid, day) VALUES (3, 2, 'Wednesday');
+INSERT INTO assignment (employeeid, enclosureid, day) VALUES (4, 3, 'Thursday');
+INSERT INTO assignment (employeeid, enclosureid, day) VALUES (4, 1, 'Thursday');
+INSERT INTO assignment (employeeid, enclosureid, day) VALUES (5, 3, 'Friday');
+INSERT INTO assignment (employeeid, enclosureid, day) VALUES (6, 4, 'Saturday');
+INSERT INTO assignment (employeeid, enclosureid, day) VALUES (6, 5, 'Sunday');
+
+--create animal
+
+INSERT INTO animal (name, type, age, enclosureid) VALUES ('Billy', 'Parrot', 12, 1);
+INSERT INTO animal (name, type, age, enclosureid) VALUES ('Bob', 'Parrot', 3, 1);
+INSERT INTO animal (name, type, age, enclosureid) VALUES ('Chris', 'Parrot', 6, 1);
+INSERT INTO animal (name, type, age, enclosureid) VALUES ('Ann', 'Elephant', 11, 2);
+INSERT INTO animal (name, type, age, enclosureid) VALUES ('Poppy', 'Elephant', 11, 2);
+INSERT INTO animal (name, type, age, enclosureid) VALUES ('Tony', 'Tiger', 8, 3);
+INSERT INTO animal (name, type, age, enclosureid) VALUES ('Dave', 'Tiger', 3, 3);
+INSERT INTO animal (name, type, age, enclosureid) VALUES ('Patrick', 'Lion', 17, 3);
+INSERT INTO animal (name, type, age, enclosureid) VALUES ('Phil', 'Lion', 3, 3);
+INSERT INTO animal (name, type, age, enclosureid) VALUES ('Annabelle', 'Lion', 8, 3);
+INSERT INTO animal (name, type, age, enclosureid) VALUES ('Bobbie', 'Lion', 5, 3);
+INSERT INTO animal (name, type, age, enclosureid) VALUES ('Peter', 'Meerkat', 5, 3);
+INSERT INTO animal (name, type, age, enclosureid) VALUES ('Chrissy', 'Meerkat', 5, 3);
+INSERT INTO animal (name, type, age, enclosureid) VALUES ('Phil', 'Meerkat', 5, 3);
+INSERT INTO animal (name, type, age, enclosureid) VALUES ('Olivia', 'Meerkat', 5, 3);
