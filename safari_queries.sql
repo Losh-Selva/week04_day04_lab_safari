e46dddaa88d3d6b60f172bee4eacfce7132ac7ee
Loshanth Selvanayagam <loshanth75@googlemail.com>
2022-12-01 17:30:22 +0000

    more extensions

--- safari_queries.sql
+++ safari_queries.sql
@@ -12,10 +12,17 @@
 -- GROUP BY animal.age, enclosure.name
 -- LIMIT 1;
 
-SELECT COUNT(animal.type), staff.name
-FROM staff INNER JOIN assignment ON staff.id = assignment.staffid
-INNER JOIN animal ON assignment.enclosureid = animal.enclosureid
--- FROM animal INNER JOIN assignment ON assignment.enclosureid = animal.enclosureid
--- INNER JOIN staff ON assignment = assignment.staffid
-GROUP BY animal.type, staff.name;
+-- SELECT COUNT(animal.type), staff.name
+-- FROM staff INNER JOIN assignment ON staff.id = assignment.staffid
+-- INNER JOIN animal ON assignment.enclosureid = animal.enclosureid
+-- -- FROM animal INNER JOIN assignment ON assignment.enclosureid = animal.enclosureid
+-- -- INNER JOIN staff ON assignment = assignment.staffid
+-- GROUP BY animal.type, staff.name;
+
+SELECT COUNT(DISTINCT animal.type) FROM animal
+INNER JOIN enclosure
+ON animal.enclosureid = enclosure.id
+INNER JOIN assignment
+ON assignment.enclosureid = enclosure.id
+WHERE assignment.staffid = 4;
 
