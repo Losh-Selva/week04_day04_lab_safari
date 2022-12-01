259ebe7cebe139661c2643ab884f6cab694e467c
Loshanth Selvanayagam <loshanth75@googlemail.com>
2022-12-01 17:21:15 +0000

    more extensions

--- safari_queries.sql
+++ safari_queries.sql
@@ -1,8 +1,21 @@
 --SELECT * FROM animal WHERE enclosureid = 1;
 --SELECT * FROM staff LEFT JOIN assignment ON staff.id = assignment.staffid WHERE assignment.enclosureid = 1;
 
-SELECT staff.name, staff.id AS staffNumber, assignment.staffid, enclosure.name AS animalEnclosure, closedForMainenance 
-FROM staff LEFT JOIN assignment ON staff.id = assignment.staffid
-LEFT JOIN enclosure 
-ON assignment.enclosureid = enclosure.id 
-WHERE closedForMainenance = true;
+-- SELECT staff.name, staff.id AS staffNumber, assignment.staffid, enclosure.name AS animalEnclosure, closedForMainenance 
+-- FROM staff LEFT JOIN assignment ON staff.id = assignment.staffid
+-- LEFT JOIN enclosure 
+-- ON assignment.enclosureid = enclosure.id 
+-- WHERE closedForMainenance = true;
+
+-- SELECT MAX(animal.age), enclosure.name 
+-- FROM animal LEFT JOIN enclosure ON enclosure.id = animal.enclosureid
+-- GROUP BY animal.age, enclosure.name
+-- LIMIT 1;
+
+SELECT COUNT(animal.type), staff.name
+FROM staff INNER JOIN assignment ON staff.id = assignment.staffid
+INNER JOIN animal ON assignment.enclosureid = animal.enclosureid
+-- FROM animal INNER JOIN assignment ON assignment.enclosureid = animal.enclosureid
+-- INNER JOIN staff ON assignment = assignment.staffid
+GROUP BY animal.type, staff.name;
+
